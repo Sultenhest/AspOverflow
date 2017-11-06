af4f1f9a22e9eb2138495798826f042e3c79f386
Simon Konstantyner <simon@konstantyner.dk>
2017-11-06 10:39:50 +0100

    sec

--- Data/Migrations/20171106092555_InitialMigration.Designer.cs
+++ Data/Migrations/20171106092555_InitialMigration.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace AspOverflow.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171106092555_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
